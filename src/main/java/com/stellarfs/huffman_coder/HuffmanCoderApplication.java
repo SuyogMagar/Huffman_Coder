1a520119665646f01ee4183471bc93b3db27dce2
Suyog Magar <hkmagar11@gmail.com>
2025-06-17 11:49:09 +0530

    Fix: Code formatting and indentation

--- src/main/java/com/stellarfs/huffman_coder/HuffmanCoderApplication.java
+++ src/main/java/com/stellarfs/huffman_coder/HuffmanCoderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HuffmanCoderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HuffmanCoderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HuffmanCoderApplication.class, args);
+    }
 
 }

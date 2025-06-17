bb7e23a7892a37d31bd59a4e8b4f9494faba8ff7
Suyog Magar <hkmagar11@gmail.com>
2025-06-17 11:45:00 +0530

    Initial commit

---
+++ src/main/java/com/stellarfs/huffman_coder/HuffmanCoderApplication.java
@@ -0,0 +1,13 @@
+package com.stellarfs.huffman_coder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuffmanCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuffmanCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stellarfs/huffman_coder/HuffmanCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellarfs.huffman_coder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuffmanCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
